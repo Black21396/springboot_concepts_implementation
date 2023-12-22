e34cc9b5d7ddd34a7a9827072bf44a58408d6c24
Black21396 <fadi.199620@gmail.com>
2023-12-22 16:11:58 +0100

    base Project structure

---
+++ src/main/java/net/fadi/springbootconcepts/SpringBootConceptsApplication.java
@@ -0,0 +1,13 @@
+package net.fadi.springbootconcepts;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootConceptsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootConceptsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/net/fadi/springbootconcepts/SpringBootConceptsApplicationTests.java
@@ -0,0 +1,13 @@
+package net.fadi.springbootconcepts;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootConceptsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
